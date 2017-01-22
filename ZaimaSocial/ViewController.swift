7fdcdaa79266099bc8c257016e4a61898e14ff00
Ahmed Zaima <a_zaima@yahoo.com>
2017-01-22 19:31:02 +0000

    Initial Commit

---
+++ ZaimaSocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZaimaSocial
+//
+//  Created by Ahmed Zaima on 22/01/2017.
+//  Copyright © 2017 Ahmed Zaima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
