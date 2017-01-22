4c816d92a67a8d3309071c9a5a234a75ec3f0f81
Ahmed Zaima <a_zaima@yahoo.com>
2017-01-22 19:58:55 +0000

    Added Firebase

---
+++ ZaimaSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  ZaimaSocial
+//
+//  Created by Ahmed Zaima on 22/01/2017.
+//  Copyright © 2017 Ahmed Zaima. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
