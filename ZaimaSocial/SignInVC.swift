caeec26f836571111b3d4481b619814f980ad4f5
Ahmed Zaima <a_zaima@yahoo.com>
2017-01-26 22:56:32 +0000

    First new comit

--- ZaimaSocial/SignInVC.swift
+++ ZaimaSocial/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -15,11 +18,33 @@ class SignInVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func facebookButtonPressed(_ sender: RoundButton) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Unable to Authenticate with facebook - \(error)")
+            }   else if result?.isCancelled == true {
+                print("User Cancelled Facebook Authentication")
+            }   else {
+                print("Successfully Authenticates Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
     }
-
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Unable to authenticate with Firebase - \(error)")
+            }   else {
+                print("Successfully Authenticated with Firebase")
+            }
+        })
+    }
+    
 
 }
 
