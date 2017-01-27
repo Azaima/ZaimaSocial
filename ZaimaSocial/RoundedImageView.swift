4a197aaa04b0cc660831d69b92043e446fcc9476
Ahmed Zaima <a_zaima@yahoo.com>
2017-01-27 08:43:48 +0000

    feed ui started

--- ZaimaSocial/RoundedImageView.swift
+++ ZaimaSocial/RoundedImageView.swift
@@ -1,23 +1,28 @@
 //
-//  FancyView.swift
+//  RoundedImageView.swift
 //  ZaimaSocial
 //
-//  Created by Ahmed Zaima on 23/01/2017.
+//  Created by Ahmed Zaima on 27/01/2017.
 //  Copyright © 2017 Ahmed Zaima. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class RoundedImageView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5
         layer.shadowOffset = CGSize(width: 1, height: 1)
-        layer.cornerRadius = 2
+
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+
     }
 
 }
